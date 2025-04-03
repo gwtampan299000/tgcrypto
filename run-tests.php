ab601fb64a663c91402747b618fe1936e2e65b63
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-04-03 05:23:06 -0700

    Update run-tests.php

--- run-tests.php
+++ run-tests.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-foreach(glob(__DIR__.DIRECTORY_SEPARATOR.'*.php') as $file){
+foreach(glob(__DIR__.DIRECTORY_SEPARATOR.'tests'.DIRECTORY_SEPARATOR.'*.php') as $file){
 	echo 'Run : ',basename($file),PHP_EOL;
 	include($file);
 }
