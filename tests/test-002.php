e72c4433f86699143c720b8f1db9879a158a6872
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2025-04-03 14:10:27 +0330

    Add files via upload

---
+++ tests/test-002.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+$start = microtime(true);
+
+for($i = 0;$i < 100;$i++){
+	$pq = tg_factorize(0x17ED48941A08F981);
+	extract($pq);
+	assert($p === 0x494C553B);
+	assert($q === 0x53911073);
+}
+
+$finish = microtime(true);
+
+echo 'It took ',$finish - $start,' seconds',PHP_EOL;
+
+?>
